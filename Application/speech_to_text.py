c94e3a8f743fac716d34a7cf4bd0887f6104f8a9
Forest Li <forestli2022@gmail.com>
2024-02-17 18:41:25 +0000

    updated web 1

--- Application/speech_to_text.py
+++ Application/speech_to_text.py
@@ -1,23 +1,20 @@
 from google.cloud import speech
 from google.oauth2 import service_account
-import pyaudio
 import base64
 
-
 LANGUAGE = 'en-US'
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 48000
 
 credentials = service_account.Credentials.from_service_account_file(
-    "../Forest/API_keys/Speech_to_Text__API_Service.json")
+    "resources/Speech_to_Text__API_Service.json")
 client = speech.SpeechClient(credentials=credentials)
 
 
 def speechToText(audio_data):
     # speech to text
     content = base64.b64decode(bytes(audio_data))
+
     audio = speech.RecognitionAudio(content=content)
     config = speech.RecognitionConfig(encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
                                       sample_rate_hertz=RATE,
